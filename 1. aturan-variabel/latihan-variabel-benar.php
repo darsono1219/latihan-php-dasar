7690517d144eed08b7b2e9a7f7af2a751e0dd3ff
darsono12 <darsono1219@gmail.com>
2025-02-27 08:00:35 +0700

    latihan php

---
+++ 1. aturan-variabel/latihan-variabel-benar.php
@@ -0,0 +1,29 @@
+<?php
+/* 
+  buatkan variabel bebas 1 kata dibawah komentar dengan kriteria sebagai berikut :
+*/
+
+// diawali huruf contoh :
+$nama
+// diawali underscore
+
+// diawali huruf 
+
+// diawali underscore
+
+
+/* 
+  buatkan variabel bebas 2 kata atau lebih dibawah komentar dengan kriteria sebagai berikut :
+*/
+
+// terdapat 2 kata, tetapi tanpa angka
+
+// terdapat 2 kata, pada awal kata kedua ada angkanya
+
+// terdapat 2 kata, pada akhir kata pertama terdapat angka
+
+// terdapat 2 kata, gunakan teknik camelcase
+
+
+
+?>
---
+++ 1. aturan-variabel/latihan-variabel-salah.php
@@ -0,0 +1,20 @@
+<?php
+/* 
+  buatkan variabel bebas 1 kata dibawah komentar dengan kriteria sebagai berikut :
+*/
+
+// diawali angka 
+
+// diawali karakter – (mines) 
+
+// menggunakan kata kunci PHP
+
+/* 
+  buatkan variabel bebas 2 kata dibawah komentar dengan kriteria sebagai berikut :
+*/
+
+// terdapat karakter @ diantara penamaan variabel
+
+
+// menggunakan spasi
+?>
